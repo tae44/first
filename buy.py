dfbfdef1598efe88465961700fa02789ba2620c8
siyang.jiang <jsy5845201314>
2016-02-25 17:59:39 +0800

    buy

---
+++ buy.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+def buy():
+    q = True
+    o, total_price, total_weight = 0, 0, 0
+    name_list, price_list, num_list, weight_list = [], [], [], []
+    while q:
+        name = input('请输入商品名称(输入q为退出): ')
+        if name == 'q': break
+        price = int(input('请输入商品单价(円): '))
+        num = int(input('请输入商品数量: '))
+        weight = int(input('请输入商品单个重量(g): '))
+        name_list.append(name)
+        price_list.append(price)
+        num_list.append(num)
+        weight_list.append(weight)
+        o += 1
+        choose = input('是否打印购物列表(y/N/q): ')
+        if choose == 'y':
+            print('-'*50)
+            for i in range(o):
+                print(i, '名称:', name_list[i], '数量:', num_list[i], '总价格(円):', price_list[i]*num_list[i], '总重量(g):', weight_list[i]*num_list[i])
+                total_price += price_list[i]*num_list[i]
+                total_weight += weight_list[i]*num_list[i]
+            print('商品总数:', i+1, '此批货总价值(円):', total_price, '此批货总重(g):', total_weight)
+            delete = int(input('是否删除某一行(输入-1不删除任何数据): '))
+            if delete == -1:
+                continue
+            else:
+                name_list.pop(delete)
+                num_list.pop(delete)
+                price_list.pop(delete)
+                weight_list.pop(delete)
+                o -= 1
+                total_price, total_weight = 0, 0
+        elif choose == 'q':
+            q = False
+        else:
+            continue
+
+def freight():
+    freight_weight = [1000, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 9500, 11000, 12000, 13000, 14000, 15000,\
+                      16000, 17000, 18000, 19000, 20000, 21000, 22000, 23000, 24000, 25000, 26000, 27000, 28000]
+    for i in freight_weight:
+        print(i)
+
+buy()
+freight()
+
