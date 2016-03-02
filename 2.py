5633c5ed0cc901f0fffa750c03f5b846078f0bfb
Jason <tae44@vip.qq.com>
2016-03-02 22:38:14 +0800

    buy-3

--- 2.py
+++ 2.py
@@ -5,24 +5,25 @@
 # 8^2 + 2^2 = 68
 # 6^2 + 8^2 = 100
 # 1^2 + 0^2 + 0^2 = 1
-#num = input('a: ')
-#
-#def loop(x,y):
-#    while True:
-#        for i in range(len(x)):
-#            y += int(x[i])**2
-#        if len(str(y)) == 1:
-#            if y == 1:
-#                print('{} is a happy number.'.format(num))
-#                break
-#            else:
-#                print('{} is not a happy number.'.format(num))
-#                break
-#        else:
-#            loop(str(y),0)
-#            break
-#
-#loop(num,0)
 
+def loop(y):
+    while True:
+        l = fin()
+        for i in range(len(l)):
+            y += int(l[i])**2
+        if len(str(y)) == 1:
+            if y == 1:
+                print('{} is a happy number.'.format(l))
+                break
+            else:break
+        else:
+            print(1111)
+            break
 
+def fin():
+    for j in range(1, 11):
+        j = str(j)
+        k = iter(j)
+        return list(k)
 
+loop(0)
