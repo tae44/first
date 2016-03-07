e55980bc1a98c34eab46dc7c8b450bd3fe3f8dff
siyang.jiang <jsy5845201314>
2016-03-07 18:02:32 +0800

    buybuy

--- t.py
+++ t.py
@@ -1,18 +1,33 @@
-class A:
-    def __init__(self, name):
-        self.name = name
+# -*- coding: utf-8 -*-
 
-    # def __repr__(self):
-    #     return self.name
-    #
-    # def __str__(self):
-    #     return 'call __str__ name is {}'.format(self.name)
-    #
-    # def __bytes__(self):
-    #     return 'call __bytes__ name is {}'.format(self.name).encode('utf-8')
+c(a,b)
+[c] + merge(mro(a),mro(b),[a,b])
+[c] + merge([a,o],[b,o],[a,b])
+[c,a] + merge([o],[b,o],[b])
+[c,a,b] + merge([o],[o])
+[c,a,b,o]
 
-a = A('Jason')
-print(a)
-# print(str(a))
-# print(repr(a))
-# print(bytes(a))
+c(b,a)
+[c] + merge(mro(b),mro(a),[b,a])
+[c] + merge([b,o],[a,o],[b,a])
+[c,b] + merge([o],[a,o],[a])
+[c,b,a] + merge([o],[o])
+[c,b,a,o]
+
+c(a,b) + b(a)
+[c] + merge(mro(a),mro(b),[a,b])
+[c] + merge([a,o],([b]+merge(mro(a),[a])),[a,b])
+[c] + merge([a,o],([b]+merge([a,o],[a]),[a,b]))
+[c] + merge([a,o],([b,a]+merge([o])),[a,b])
+[c] + merge([a,o],[b,a,o],[a,b])
+
+c(b,a) + b(a)
+[c] + merge(mro(b),mro(a),[b,a])
+[c] + merge([a,o],([b]+merge(mro(a),[a])),[b,a])
+[c] + merge([a,o],([b]+merge([a,o],[a])),[b,a])
+[c] + merge([a,o],([b,a]+merge([o])),[b,a])
+[c] + merge([a,o],[b,a,o],[b,a])
+[c,b] + merege([a,o],[a,o],[a])
+[c,b,a] + merge([o],[o])
+[c,b,a,o]
+
