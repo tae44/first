465dff3c916b8621b541c3cab773fd89edbe317c
Jason <tae44@vip.qq.com>
2016-03-17 17:53:30 +0800

    excel

--- buy-2.py
+++ buy-2.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
+from openpyxl import Workbook
+import datetime
 
 def type(fn):
     def wrap(*args, **kwargs):
@@ -37,16 +40,17 @@ class Buy:
     def print_table(self):
         print('序号', '\t', '名称', '\t', '价格', '\t', '数量', '\t', '重量', '\t')
         for i in range(len(self.name_table)):
-            print('{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}'.\
+            print('{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}'.
                 format(i, self.name_table[i],self.price_table[i],self.number_table[i],self.weight_table[i]))
 
     def print_detailed_table(self):
         print('名称', '\t', '总价格', '\t', '总重量', '\t', '重量占比', '\t', '商品加成', '\t', '商品估算成本')
         for i in range(len(self.name_table)):
-            print('{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}\t\t{5}'.\
-                format(self.name_table[i], self.total_price[i], self.total_weight[i], 1,2,3))
-        print('-' * 20)
-        print('总价值: {0}      总重量: {1}'.format(   ))
+            print('{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}\t\t{5}'.
+                format(self.name_table[i], self.total_price[i], self.total_weight[i], round(self.total_weight[i]/sum(self.total_weight)*100, 2),
+                       2,3))
+        print('-' * 30)
+        print('总价值: {0}      总重量: {1}'.format(sum(self.total_price),sum(self.total_weight)))
 
     def delete_item(self, i):
         self.name_table.pop(i)
@@ -55,6 +59,19 @@ class Buy:
         self.weight_table.pop(i)
         self.total_weight.pop(i)
         self.total_price.pop(i)
+
+    def save_excel(self):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = '成本计算单'
+        ws.append(['名称:', '数量:', '总价格(円):', '总重量(g):', '重量占比(%):', '商品加成(円):', '商品估算成本价(円):'])
+        # ws.append(['商品总数:', i+1, '总价值(円):', total_price, '总重量(g):', total_weight])
+        wb.save(r'E:\%s.xlsx' % datetime.datetime.utcnow().strftime("%Y-%m-%d-%H-%M"))
+
+        # TODO 保存excel
+
+    def exit_buy(self):
+        sys.exit('exit...')
 
 class Japen(Buy):
     def enter_data(self):
@@ -71,6 +88,7 @@ class Japen(Buy):
             self.if_del()
         elif print_choose == 'd':
             super(Japen, self).print_detailed_table()
+            self.weight_price()
         else:
             self.enter_data()
 
@@ -83,14 +101,22 @@ class Japen(Buy):
         else:
             self.enter_data()
 
-    freight_weight = [1000, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 9500, 10000, 11000, 12000, 13000, 14000, 15000,\
+    def weight_price(self):
+        freight_weight = [1000, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 9500, 10000, 11000, 12000, 13000, 14000, 15000,
                       16000, 17000, 18000, 19000, 20000, 21000, 22000, 23000, 24000, 25000, 26000, 27000, 28000]
-    freight_price = [1500, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 3750, 3950, 4150, 4350, 4550, 4750,\
+        freight_price = [1500, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 3750, 3950, 4150, 4350, 4550, 4750,
                      4950, 5150, 5350, 5550, 5750, 5950, 6150, 6350, 6550, 6750, 6950, 7150, 7350]
+        for i, v in enumerate(freight_weight):
+            if sum(self.total_weight) < v:
+                print('此批货物估算重量为{0}g,运费估算为{1}円.'.format(v, freight_price[i]))
+                break
+        if_quit = input('是否保存并退出(y/N): ')
+        if if_quit == 'y':
+            super(Japen, self).save_excel()
+            super(Japen, self).exit_buy()
+        else:
+            self.enter_data()
 
 j = Japen()
 while True:
     j.enter_data()
-
-
-    # TODO 计算总价值和总重量
--- watchdog.py
+++ watchdog.py
@@ -1,20 +1,86 @@
-import time
+import os
+import logging
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-class Myhandler(FileSystemEventHandler):
+logging.basicConfig(level=logging.INFO)
+
+
+class Watcher(FileSystemEventHandler):
+    def __init__(self, filename, matcher):
+        super(Watcher, self).__init__()
+        self.filename = filename
+        self.matcher = matcher
+        self.fd = None
+        self.offset = 0
+        if os.path.isfile(self.filename): # 如果这是一个存在的文件
+            self.fd = open(self.filename)
+            self.offset = os.path.getsize(self.filename) # getsize可以看到文件的字符数
+
+    def __close(self):
+        if self.fd is not None and not self.fd.closed:
+            self.fd.close()
+            self.fd = None
+
+    def __open(self):
+        self.fd = open(self.filename)
+        self.offset = 0
+
+    def __read(self):
+        size = os.path.getsize(self.filename)
+        if size < self.offset:
+            self.offset = 0
+        self.fd.seek(self.offset, 0)
+        for line in self.fd:
+            line.rstrip('\n')
+            match = getattr(self.matcher, 'match', lambda x: False)
+            if match(line):
+                logging.info("match {0}".format(line))
+            logging.info(line)
+        self.offset = self.fd.tell()
+
+    @property
+    def dirname(self):
+        return os.path.dirname(os.path.abspath(self.filename))
+
+    def on_deleted(self, event):
+        if event.src_path == self.filename:
+            logging.info("{0} deleted, close it".format(self.filename))
+            self.__close()
+
+    def on_moved(self, event):
+        if event.src_path == self.filename:
+            logging.info("{0} move out, close it".format(self.filename))
+            self.__close()
+        if event.dest_path == self.filename and not event.is_directory:
+            logging.info("{0} move in, open it".format(self.filename))
+            self.__close()
+            self.__open()
+            self.__read()
+
+    def on_created(self, event):
+        if event.src_path == self.filename and not event.is_directory:
+            logging.info("{0} created, open it".format(self.filename))
+            self.__close()
+            self.__open()
+            self.__read()
+
     def on_modified(self, event):
-        if event.src_path == '/Users/a123/Desktop/456.txt':
-            print('file {} is changed!'.format(event.src_path))
+        if event.src_path == self.filename:
+            logging.info("{0} modified".format(event.src_path))
+            self.__read()
+
 
 if __name__ == '__main__':
-    event_handler = Myhandler()
+    import sys
     observer = Observer()
-    observer.schedule(event_handler, path='.', recursive=False)
+    watcher = Watcher(sys.argv[1], None)
+    w2 = Watcher(sys.argv[2], None)
+    observer.schedule(watcher, watcher.dirname, recursive=False)
+    observer.schedule(w2, w2.dirname, recursive=False)
     observer.start()
     try:
-        while True:
-            time.sleep(1)
+        observer.join()
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
